ccc962c755ca23d0bbcb47ad141afa6dfacaecb5
Stelian Iancu <stelian.iancu@gmail.com>
2020-06-13 18:30:45 +0200

    implement the mostBlogs function

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -30,8 +30,29 @@ const favoriteBlog = (blogs) => {
   }
 }
 
+const mostBlogs = (blogs) => {
+  const blogsByAuthors = _.groupBy(blogs, 'author')
+  // blogsByAuthors is a dictionary-like object where the key is the author name
+  // and the value is an array of blogs belonging to that author
+
+  const a = _.mapValues(blogsByAuthors, (arrayOfBlogs, author) => {
+    return {
+      author: author,
+      blogs: arrayOfBlogs.length
+    }
+  })
+  // a is a dictionary-like object where the key is an author and the value
+  // is another object with 'author' -> author_name and 'blogs' -> number_of_blogs
+
+  const b = _.map(a, key => key)
+  // b is an array of a; needed for the maxBy below
+
+  return _.maxBy(b, 'blogs')
+}
+
 module.exports = {
   // dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
