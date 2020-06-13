4b6de63c03176e5af805adca1ec71df0a62b1984
Stelian Iancu <stelian.iancu@gmail.com>
2020-06-13 16:11:44 +0200

    exercises 4.1 - 4.5

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -1,7 +1,42 @@
-const dummy = (blogs) => {
-  return 1
+// const dummy = (blogs) => {
+//   return 1
+// }
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item
+  }
+
+  const blogsLikes = blogs.map(blog => blog.likes)
+  return blogsLikes.length === 0
+    ? 0
+    : blogsLikes.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {}
+  }
+
+  let max = 0
+  let maxIndex = 0
+  blogs.forEach((blog, index) => {
+    if (blog.likes > max) {
+      max = blog.likes
+      maxIndex = index
+    }
+  })
+
+  const maxBlog = blogs[maxIndex]
+  return {
+    title: maxBlog.title,
+    author: maxBlog.author,
+    likes: maxBlog.likes
+  }
 }
 
 module.exports = {
-  dummy
+  // dummy,
+  totalLikes,
+  favoriteBlog
 }
